087c5de4ec3dd12c3d5cc47d6a14b6d4f5ba34a6
Michelle Morin <morin2m@gmail.com>
2020-04-24 14:17:18 -0700

    change position on page

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ function Header(){
   const headerStyles = {
     marginLeft: '10vw',
     marginRight: '10vw',
-    position: 'fixed',
+    position: 'absolute',
     top: '5vh',
     left: '0',
     width: '80vw',
@@ -15,15 +15,17 @@ function Header(){
     flexWrap: 'wrap',
     borderTop: '3px solid #388087',
     borderBottom: '3px solid #388087',
+    backgroundColor: '#f6f6f2',
     color:  'rgb(227, 138, 89)',
-    backgroundColor: 'transparent',
     fontSize: '1.5rem'
   }
 
   return (
     <React.Fragment>
       <div style={headerStyles}>
-        <h1>REACT TAPROOM</h1>
+        <header>
+          <h1>REACT TAPROOM</h1>
+        </header>
       </div>
     </React.Fragment>
   );
