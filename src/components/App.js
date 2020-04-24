3f90619593d393cd95e1530d192e17ab91db71e2
Michelle Morin <morin2m@gmail.com>
2020-04-24 14:17:00 -0700

    import Footer component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './Header.js';
 import TapControl from './TapControl.js';
+import Footer from './Footer.js';
 
 function App() {
   return (
     <React.Fragment>
       <Header />
       <TapControl />
+      <Footer />
     </React.Fragment>
   );
 }
