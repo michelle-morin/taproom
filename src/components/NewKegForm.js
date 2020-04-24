8c18a05be7d1d89b2d6500412855b8c53e54564e
Michelle Morin <morin2m@gmail.com>
2020-04-24 11:41:08 -0700

    add inline styles for new keg form component

--- src/components/NewKegForm.js
+++ src/components/NewKegForm.js
@@ -9,27 +9,37 @@ function NewKegForm(props) {
     props.onNewKegCreation({name: event.target.name.value, brewery: event.target.brewery.value, alcoholContent: event.target.alcoholContent.value, price: event.target.price.value, pintQuantity: 124, id: v4()});
   }
 
+  const newKegFormStyles = {
+    marginTop: '2%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    flexWrap: 'wrap'
+  }
+
   return (
     <React.Fragment>
-      <form onSubmit={handleNewKegFormSubmission}>
-        <input
-          type='text'
-          name='name'
-          placeholder='brew name' />
-        <input
-          type='text'
-          name='brewery'
-          placeholder='brewery name' />
-        <input
-          type='number'
-          name='alcoholContent'
-          placeholder='ABV' />
-        <input
-          type='number'
-          name='price'
-          placeholder='price' />
-        <button type="submit">ADD KEG</button>
-      </form>
+      <div style={newKegFormStyles}>
+        <form onSubmit={handleNewKegFormSubmission}>
+          <input
+            type='text'
+            name='name'
+            placeholder='brew name' />
+          <input
+            type='text'
+            name='brewery'
+            placeholder='brewery name' />
+          <input
+            type='number'
+            name='alcoholContent'
+            placeholder='ABV' />
+          <input
+            type='number'
+            name='price'
+            placeholder='price' />
+          <button className="new-keg-button" type="submit">ADD KEG</button>
+        </form>
+      </div>
     </React.Fragment>
   );
 }
