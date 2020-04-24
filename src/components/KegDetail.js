d8b39c42bf27d9424624cb0e131c8040f37a0b15
Michelle Morin <morin2m@gmail.com>
2020-04-24 15:32:23 -0700

    add delete button that is visible/selectable  when keg is empty

--- src/components/KegDetail.js
+++ src/components/KegDetail.js
@@ -3,12 +3,18 @@ import BeerImage from './BeerImage';
 import PropTypes from 'prop-types';
 
 function KegDetail(props){
-  const { keg } = props;
+  const { keg, onClickingDelete } = props;
 
   const kegDetailStyles = {
     marginTop: '2%',
     textAlign: 'center'
   }
+
+  function isDeletable(quantity) {
+    if (quantity <= 0) {
+      return <button className="delete-button" onClick={()=> onClickingDelete(keg.id)}>DELETE KEG</button>
+    }
+  };
 
   return (
     <React.Fragment>
@@ -17,6 +23,7 @@ function KegDetail(props){
         <p>{keg.alcoholContent} ABV</p>
         <p>${keg.price} /pint</p>
         <p>pints in keg: {keg.pintQuantity}</p>
+        {isDeletable(keg.pintQuantity)}
         <BeerImage />
       </div>
     </React.Fragment>
@@ -24,7 +31,8 @@ function KegDetail(props){
 }
 
 KegDetail.propTypes = {
-  keg: PropTypes.object
+  keg: PropTypes.object,
+  onClickingDelete: PropTypes.func
 }
 
 export default KegDetail;
