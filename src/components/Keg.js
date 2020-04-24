bc5efcdc6daac8442f21a305022381fbce2b5672
Michelle Morin <morin2m@gmail.com>
2020-04-24 11:40:53 -0700

    update inline styles for keg component

--- src/components/Keg.js
+++ src/components/Keg.js
@@ -2,12 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Keg(props) {
+
+  const kegStyles = {
+    textAlign: 'center',
+    border: '2px solid #388087',
+    width: '200px',
+    height: '200px',
+    padding: '2%',
+    margin: '1%'
+  }
+
   return(
     <React.Fragment>
-      <div onClick = {()=> props.whenKegClicked(props.id)}>
-        <h3>{props.name} | {props.brewery}</h3>
-        <p>{props.alcoholContent} ABV</p>
-        <p>${props.price} /pint</p>
+      <div style={kegStyles}>
+        <div onClick = {()=> props.whenKegClicked(props.id)}>
+          <h3 className="beer-name">{props.name}</h3>
+          <p>{props.brewery}</p>
+          <p>{props.alcoholContent} ABV</p>
+          <p>${props.price} /pint</p>
+        </div>
       </div>
     </React.Fragment>
   );
