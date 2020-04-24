e16b9f65eecb6d59ea7fa1b9b285e5e8e0fa517b
Michelle Morin <morin2m@gmail.com>
2020-04-24 08:42:25 -0700

    add keg component with placeholders for props

---
+++ src/components/Keg.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Keg(){
+  return(
+    <React.Fragment>
+      <h3>name | brewery name</h3>
+      <p>alcoholContent</p>
+      <p>price</p>
+    </React.Fragment>
+  );
+}
+
+export default Keg;
