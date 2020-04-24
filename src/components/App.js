7a580f9b0e6702557fdb662ef64b2abc8a8c08bc
Michelle Morin <morin2m@gmail.com>
2020-04-24 08:34:38 -0700

    update App component to include header and tapcontrol components

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'Header' from './Header.js';
+import 'TapControl' from './TapControl.js';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header />
+      <TapControl />
+    </React.Fragment>
+  );
+}
+
+export default App;
