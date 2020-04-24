e456fb6ea686f7076e1307236839a2b7b52df280
Michelle Morin <morin2m@gmail.com>
2020-04-24 16:29:16 -0700

    Updates

---
+++ precache-manifest.09c561483823901c30a214222086ae18.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4caf752dc2cf6f86315d0bf6c099297b",
+    "url": "/taproom/index.html"
+  },
+  {
+    "revision": "e7b420ff5d76ac91b423",
+    "url": "/taproom/static/css/main.5129cc27.chunk.css"
+  },
+  {
+    "revision": "f521e073d6c4b5f8cc27",
+    "url": "/taproom/static/js/2.677bb138.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/taproom/static/js/2.677bb138.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7b420ff5d76ac91b423",
+    "url": "/taproom/static/js/main.c743421d.chunk.js"
+  },
+  {
+    "revision": "ad49214af0cad1da5e2b",
+    "url": "/taproom/static/js/runtime-main.72649f39.js"
+  },
+  {
+    "revision": "b3792d9b0022d8b00bdc3ca825b73bc5",
+    "url": "/taproom/static/media/beer.b3792d9b.jpg"
+  }
+]);
