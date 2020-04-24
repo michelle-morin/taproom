737c071da252bcacaca0e077c1e3c29e23623b6e
Michelle Morin <morin2m@gmail.com>
2020-04-24 09:03:56 -0700

    add handleClick method to toggle a boolean when updating state

--- src/components/TapControl.js
+++ src/components/TapControl.js
@@ -11,17 +11,27 @@ class TapControl extends React.Component {
     };
   }
 
+  handleClick = () => {
+    this.setState(prevState => ({
+      formVisibleOnPage: !prevState.formVisibleOnPage
+    }));
+  }
+
   render(){
     let currentlyVisibleState = null;
+    let buttonText = null;
     if(this.state.formVisibleOnPage) {
       currentlyVisibleState = <NewKegForm />
+      buttonText = "return to tap list";
     } else {
       currentlyVisibleState = <TapList />
+      buttonText = "add a keg";
     }
 
     return(
       <React.Fragment>
         {currentlyVisibleState}
+        <button onClick={this.handleClick}>{buttonText}</button>
       </React.Fragment>
     );
   }
